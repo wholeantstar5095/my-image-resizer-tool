98ec519a470a20ab7329d7b3f64e19061f9fc391
Sage Brown <shaynemcmilleonlejiqo73@gmail.com>
2026-04-22 18:36:16 -0300

    Initial commit: image resizer

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# my-image-resizer-tool
-A simple image resizer project
+# Image Resizer
+
+A simple image resizer project.
+\n
+## Getting Started
+
+```bash
+python image.py
+```
---
+++ image.py
@@ -0,0 +1,10 @@
+"""
+Image Resizer - Main module.
+"""
+
+def main():
+    print("Image Resizer is running...")
+    print("Version 0.1.0")
+
+if __name__ == "__main__":
+    main()
